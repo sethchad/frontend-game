ee7bff5801ce9457b0fec586fb89358bc1159bc8
Chadwick Seth W <seth.chadwick@gmail.com>
2021-01-20 14:58:23 -0600

    new game button creates an array of 10 random numbers between 0 and 3 to use for the game

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
+
+// Event listeners and fuctions for 
 let gameButton = document.querySelectorAll('.gameButton')
 
+// Dims button on mouse down
+gameButton.forEach(gameButton => gameButton.addEventListener('mousedown', function(e) {
+    e.target.style.opacity = "0.5"}));
+
+// Returns button to full color on mouseup
+gameButton.forEach(gameButton => gameButton.addEventListener('mouseup', function(e) {
+    e.target.style.opacity = "1"}));
+
+// Game Play
 gameButton.forEach(gameButton => gameButton.addEventListener('click', clickHandler))
 
 function clickHandler(e) {
     e.preventDefault();
     console.log(Number(e.target.dataset.value));
 
-    e.target.style.opacity = "0.5";
+    
 }
 
+
+// New Game button
 document.querySelector('button').addEventListener('click', newGameHandler)
 
 function newGameHandler(e) {
     e.preventDefault();
     console.log(e);
+
+    let gameSequence = [];
+
+    for (i = 0; i < 10; i++) {
+        random = Math.floor(Math.random() * Math.floor(4));
+        gameSequence.push(random);
+    }
+
+    console.log(gameSequence)
 }
