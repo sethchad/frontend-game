78987beccef036d692815761c37ff331e4e4fd1d
Chadwick Seth W <seth.chadwick@gmail.com>
2021-01-20 16:11:12 -0600

    script file reorganized to initialize variables and after the New Game button is depressed instead of on page load

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
 
-// Event listeners and fuctions for 
-let gameButton = document.querySelectorAll('.gameButton')
-
-// Dims button on mouse down
-gameButton.forEach(gameButton => gameButton.addEventListener('mousedown', function(e) {
-    e.target.style.opacity = "0.5"}));
-
-// Returns button to full color on mouseup
-gameButton.forEach(gameButton => gameButton.addEventListener('mouseup', function(e) {
-    e.target.style.opacity = "1"}));
-
-// Game Play
-gameButton.forEach(gameButton => gameButton.addEventListener('click', clickHandler))
+// New Game button
+document.querySelector('button').addEventListener('click', newGameHandler)
 
 let gameSequence = [];
 let playerSequence = [];
 let round = 0;
 
+function newGameHandler(e) {
+    e.preventDefault();
+
+    let gameButton = document.querySelectorAll('.gameButton')
+
+    // Dims button on mouse down
+    gameButton.forEach(gameButton => gameButton.addEventListener('mousedown', function(e) {
+        e.target.style.opacity = "0.5"}));
+    
+    // Returns button to full color on mouseup
+    gameButton.forEach(gameButton => gameButton.addEventListener('mouseup', function(e) {
+        e.target.style.opacity = "1"}));
+
+    // Game Play Listeners
+    gameButton.forEach(gameButton => gameButton.addEventListener('click', clickHandler))
+
+    // Resets variables
+    round = 0;
+    gameSequence = [];
+    playerSequence = [];
+    document.querySelector('.outcome').innerText = "";
+
+    // Generates game sequence
+    for (i = 0; i < 10; i++) {
+        random = Math.floor(Math.random() * Math.floor(4));
+        gameSequence.push(random);
+    }
+
+    console.log(gameSequence)
+}
+
+
 function clickHandler(e) {
     e.preventDefault();
-    console.log(Number(e.target.dataset.value));
 
     if (round < 10) 
     {
         playerSequence.push(Number(e.target.dataset.value))
 
         for(i = 0; i < playerSequence.length; i++) {
-            if (playerSequence[i] == gameSequence[i]) {
-                console.log("correct")
-                console.log("player sequence length:", playerSequence.length)
+            if (playerSequence[i] != gameSequence[i]) {
+                document.querySelector('.outcome').innerText = "wrong, game over";
             } 
-            else
-            {
-                console.log("wrong, game over")
-            }
         }
     }
     else
@@ -41,26 +56,6 @@ function clickHandler(e) {
         console.log("game over")
     }
 
-
     console.log("player sequence", playerSequence)
-}
-
-
-// New Game button
-document.querySelector('button').addEventListener('click', newGameHandler)
-
-function newGameHandler(e) {
-    e.preventDefault();
-    console.log(e);
-
-    round = 0;
-    gameSequence = [];
-    playerSequence = [];
-
-    for (i = 0; i < 10; i++) {
-        random = Math.floor(Math.random() * Math.floor(4));
-        gameSequence.push(random);
-    }
-
-    console.log(gameSequence)
+    
 }
